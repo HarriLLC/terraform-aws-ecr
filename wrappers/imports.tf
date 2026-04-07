922ebb3fa8741e6585754596e88c5e26cf6cef30
AhmadoshHamad <1212621@student.birzeit.edu>
2026-04-07 21:17:20 +0300

    Fix Error

--- wrappers/imports.tf
+++ wrappers/imports.tf
@@ -1,17 +1,17 @@
 import {
   for_each = {for k,v in var.items : k => v if v.enable_import}
   to       = module.wrapper[each.key].aws_ecr_repository.this[0]
-  id       = each.value
+  id       = each.key
 }
 
 import {
   for_each = {for k,v in var.items : k => v if v.enable_import}
   to       = module.wrapper[each.key].aws_ecr_repository_policy.this[0]
-  id       = each.value
+  id       = each.key
 }
 
 import {
   for_each = {for k,v in var.items : k => v if v.enable_import}
   to       = module.wrapper[each.key].aws_ecr_lifecycle_policy.this[0]
-  id       = each.value
+  id       = each.key
 }
